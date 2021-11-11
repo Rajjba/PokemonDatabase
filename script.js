003f760c53577d8761864ddae35fb5544959c579
Rayan Hadjari <rayan.hadjari@learnet.se>
2021-11-11 12:08:31 +0100

    added images

--- script.js
+++ script.js
@@ -15,8 +15,43 @@ document.querySelector("form").addEventListener("submit", LogIn);
 function LogIn(event) {
   event.preventDefault();
   let PokeS = document.querySelector("#PokemonS").value;
-  document.querySelector("#Name").innerHTML = Data[PokeS]["name"];
-  document.querySelector("#Type").innerHTML = Data[PokeS]["types"];
-  document.querySelector("#Evos").innerHTML = Data[PokeS]["evos"];
-  document.querySelector("#Color").innerHTML = Data[PokeS]["color"];
+  document.querySelector("#Name").innerHTML = "Name: " + Data[PokeS]["name"];
+  document.querySelector("#Type").innerHTML = "Types: " + Data[PokeS]["types"];
+  document.querySelector("#Evos").innerHTML = "Evos: " + Data[PokeS]["evos"];
+  document.querySelector("#ID").innerHTML = "ID: " + Data[PokeS]["num"];
+  //document.querySelector("#Color").innerHTML = Data[PokeS]["color"];
+
+  let col = Data[PokeS]["color"];
+  switch (col) {
+    case "Green":
+      document.body.style.backgroundColor = "#1efc4e";
+      break;
+    case "Red":
+      document.body.style.backgroundColor = "red";
+      break;
+    case "Blue":
+      document.body.style.backgroundColor = "blue";
+      break;
+    case "Black":
+      document.body.style.backgroundColor = "black";
+      break;
+    case "Yellow":
+      document.body.style.backgroundColor = "yellow";
+      break;
+    case "Purple":
+      document.body.style.backgroundColor = "purple";
+      break;
+    case "Brown":
+      document.body.style.backgroundColor = "brown";
+      break;
+    case "Pink":
+      document.body.style.backgroundColor = "pink";
+      break;
+    case "White":
+      document.body.style.backgroundColor = "white";
+      break;
+    case "Gray":
+      document.body.style.backgroundColor = "gray";
+      break;
+  }
 }
